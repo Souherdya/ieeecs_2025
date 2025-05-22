d94b4018332f367c3e103e765f89b128ae5034fd
Souherdya Sarkar <souherdya.iema@gmail.com>
2025-05-22 20:49:11 +0530

    Image fit made

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,11 +3,11 @@ import friends from '../assets/friends.jpeg'
 
 export const Image = () => {
   return (
-    <section className='relative overflow-hidden bg-[rgba(0,0,0,0)] flex justify-center items-center w-full h-screen'>
+    <section className='relative overflow-hidden bg-[rgba(0,0,0,0)] flex justify-center items-center w-full h-[27vh] sm:h-[70vh] lg:h-[100vh]'>
   <img
     src={friends}
     alt="group pic"
-    className='w-[100vh] h-[100vw] object-cover rotate-[270deg]'
+    className='w-[100vh] h-[100vw] object-contain sm:object-cover rotate-[270deg]'
     draggable={false}
   />
   <div className="absolute inset-0 bg-gradient-to-b from-black/70 to-black/20 pointer-events-none" />
